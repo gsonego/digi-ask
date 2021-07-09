fccafc2433eacce5b55ddff395e1a8c9437e8918
Guilherme Sonego Neto <gsonego@gmail.com>
2021-07-09 16:53:53 +0100

    Changing button behavior for Admin room

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -8,8 +8,12 @@ import { Question } from '../components/Question';
 import { Logo } from '../components/Logo';
 
 import deleteImg from '../assets/images/delete.svg';
+
 import checkImg from '../assets/images/check.svg';
+import checkImgOn from '../assets/images/check_on.svg';
+
 import answerImg from '../assets/images/answer.svg';
+import answerImgOn from '../assets/images/answer_on.svg';
 
 import '../styles/room.scss';
 
@@ -24,28 +28,33 @@ export function AdminRoom() {
   const { title, questions } = useRoom(roomId);
 
   async function handleEndRoom() {
-    await database.ref(`rooms/${roomId}`).update({ 
-      endedAt: new Date(),  
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date(),
     });
 
     history.push('/');
   }
 
-  async function handleDeleteQuestion(questionId: string){
-    if (window.confirm("Are you sure you want to delete this question ?")){
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm("Are you sure you want to delete this question ?")) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
   }
 
-  async function handleQuestionAsAsAnswered(questionId: string) {
-    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
-      isAnswered: true
+  async function handleQuestionAsAsAnswered(questionId: string, currentState: boolean) {
+    const ref = database.ref(`rooms/${roomId}/questions/${questionId}`);
+    await ref.update({
+      isHighlighted: false,
+      isAnswered: !currentState
     });
   }
 
-  async function handleHighlightQuestion(questionId: string) {
-    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
-      isHighlighted: true
+  async function handleHighlightQuestion(questionId: string, isAnswered: boolean, isHighlighted: boolean) {
+    if (isAnswered) return;
+
+    const ref = database.ref(`rooms/${roomId}/questions/${questionId}`);
+    await ref.update({
+      isHighlighted: !isHighlighted
     });
   }
 
@@ -64,11 +73,11 @@ export function AdminRoom() {
       <main className="content">
         <div className="room-title">
           <h1>Room: {title}</h1>
-          { questions.length > 0 && <span>{questions.length} { questions.length === 1 ? 'question' : 'questions' }</span> }
+          {questions.length > 0 && <span>{questions.length} {questions.length === 1 ? 'question' : 'questions'}</span>}
         </div>
 
         <div className="question-list">
-          { 
+          {
             questions.map(question => {
               return (
                 <Question
@@ -78,24 +87,19 @@ export function AdminRoom() {
                   isAnswered={question.isAnswered}
                   isHighlighted={question.isHighlighted}
                 >
-                { !question.isAnswered && (
-
-                <>
                   <button
-                  type="button"
-                  onClick={() => handleQuestionAsAsAnswered(question.id)}
+                    type="button"
+                    onClick={() => handleQuestionAsAsAnswered(question.id, question.isAnswered)}
                   >
-                  <img src={checkImg} alt="Mark as answered" />
+                    <img src={ question.isAnswered ? checkImgOn : checkImg } alt="Mark as answered" />
                   </button>
 
                   <button
-                  type="button"
-                  onClick={() => handleHighlightQuestion(question.id)}
+                    type="button"
+                    onClick={() => handleHighlightQuestion(question.id, question.isAnswered, question.isHighlighted)}
                   >
-                  <img src={answerImg} alt="Highlight question" />
+                    <img src={ question.isHighlighted ? answerImgOn : answerImg } alt="Highlight question" />
                   </button>
-                </>
-                )}
 
                   <button
                     type="button"
@@ -104,7 +108,7 @@ export function AdminRoom() {
                     <img src={deleteImg} alt="Delete question" />
                   </button>
                 </Question>
-              )  
+              )
             })
           }
         </div>
